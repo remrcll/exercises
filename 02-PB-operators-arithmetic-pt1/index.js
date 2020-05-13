b94d893193eea2c377ea684cba58a4fa91241306
mrcll <marcell.csokas@pm.me>
2020-05-13 14:54:26 +0200

    mrcllSolution

---
+++ 02-PB-operators-arithmetic-pt1/index.js
@@ -0,0 +1,59 @@
+let x = 4;
+let y = 5;
+
+// 1. Add x and y.
+console.log(x + y);
+
+// 2. Subtract y from x. Then, subtract x from y.
+console.log(y - x);
+console.log(x - y);
+
+// 3. Multiply x and y.
+
+console.log(x * y);
+
+// 4. Divide x by y.
+
+console.log(x / y);
+
+/* 5. Declare another variable "z" with value "10". 
+Multiply x and y. Then, divide the result by z. 
+Store the result in a new variable named "resultOne". 
+Print "resultOne" to the console.
+*/
+let z = 10;
+let resultOne = (x * y) / z;
+
+console.log(resultOne);
+
+//6. Declare two variables: "a" with value 15, and "b" with value 9. Divide a by b.
+
+let a = 15;
+let b = 9;
+
+console.log(a / b);
+
+// Declare another variable "c" with value 20. Add a and b, then multiply the result by c. Store the result in variable "resultTwo". Print "resultTwo" to the console.
+
+let c = 20;
+let resultTwo = (a + b) / c;
+console.log(resultTwo);
+
+//8. Increment a.
+
+console.log(a + 1);
+
+//9. Decrement b.
+
+console.log(b - 1);
+
+//10. Subtract a from b and store this in a new variable "d". Add d and c.
+
+let d = a - b;
+console.log(d + c);
+
+//11. Print the remainder when "resultOne" is divided by "resultTwo" to the console.
+
+console.log(resultOne % resultTwo);
+
+
