ac3821f2fc4c5ea38f48c54c0ad276713ef7deb1
mrcll <marcell.csokas@pm.me>
2020-05-13 09:29:39 +0200

    first exercise ready

---
+++ PB-language-statements/statements.js
@@ -0,0 +1,7 @@
+console.log('Hello World');
+// console.log('This is my first JS file.');
+
+/* 
+    1. Answer: no it doesn't print.
+    2. Answer: node always  expects two parenthesis, an opening and closing. Otherwise our statement brakes. Syntax error, unexpected string. 
+*/
