d9409adec31dd002b83c7f476686d8ffd4b91740
mrcll <marcell.csokas@pm.me>
2020-05-21 12:14:09 +0200

    11-mrcll-solution

---
+++ 11-PB-language-pitfalls/11-mrcll-solution.js
@@ -0,0 +1,33 @@
+// 1. NaN
+// Question: Comment your answer in your js file: What does NaN stand for?
+
+// NaN = Not a number.
+
+// Check if "hello" is NaN.
+console.log(isNaN("hello"));
+
+// Check if 3 is NaN.
+console.log(isNaN(3));
+
+// What is the type of NaN?
+console.log(typeof(NaN));
+
+
+// 2. Rounding
+// console.log(0.1 * 0.2);
+// The above in JavaScript will print: 0.020000000000000004. Use a method to make sure that only 0.02 is printed.
+
+console.log((0.1 * 0.2).toFixed(2));
+
+// 3. Infinity
+// Divide Infinity by 0.
+
+console.log(Infinity / 0);
+
+// Divide Infinity by Infinity.
+
+console.log(Infinity / Infinity);
+
+// Divide 1 by 0.
+
+console.log(1 / 0); 
