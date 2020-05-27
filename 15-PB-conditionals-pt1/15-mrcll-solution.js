58da97a9ef843b53d045ca76250e75d3c6498c3d
mrcll <marcell.csokas@pm.me>
2020-05-27 09:51:57 +0200

    final solutions after discussion

--- 15-PB-conditionals-pt1/15-mrcll-solution.js
+++ 15-PB-conditionals-pt1/15-mrcll-solution.js
@@ -6,19 +6,24 @@ let myNum2 = 66;
 let myNum3 = 99;
 
 if ((myNum1 >= 50 && myNum1 <= 99) || (myNum2 >= 50 && myNum2 <= 99)) {
-    console.log("true");
+    console.log(true);
+}
+
+if ((50 <= myNum1 <= 99) || (50 <= myNum2 <= 99)) {
+    console.log(true);
 }
 
 // First I wanted to do:
 // if (myNum1 || myNum2 >= 50 && myNum1 || myNum2 <= 99) {
 //     console.log("true");
 // }
+
 // but it doesn't work properly, doesn't matter if I put things in brackets or not, and I don't understand why...
 
 // 2. Check if three given integer values are in the range 50 to 99 (inclusive). Print true if one or more of them are in the range.
 
 if ((myNum1 >= 50 && myNum1 <= 99) || (myNum2 >= 50 && myNum2 <= 99) || (myNum3 >= 50 && myNum3 <= 99)) {
-    console.log("true");
+    console.log(true);
 }
 
 // 3. Declare the variables a, b and c, and give them number values. Check which one out of the three variables has the largest value and print it to the console. 
@@ -93,6 +98,7 @@ if ((myNum1 + myNum2) >= 50 && (myNum1 + myNum2) <= 80) {
 //or
 
 let mySum = myNum1 + myNum2;
+let mySub = myNum1 - myNum2;
 
 if (mySum >= 50 && mySum <= 80) {
     console.log(65);
@@ -102,7 +108,7 @@ if (mySum >= 50 && mySum <= 80) {
 
 //6. Check whether the sum of two integers is 8, or whether their difference is 8. If one of these is the case, print true.
 
-if ((mySum === 8) || (myNum1 - myNum2 === 8)) {
+if ((mySum === 8) || (mySub === 8)) {
     console.log(true);
 }
 
@@ -110,8 +116,8 @@ if ((mySum === 8) || (myNum1 - myNum2 === 8)) {
 
 let d = 15;
 
-if ((c === 15) || (d === 15) || ((d + c) === 15)) {
-    console.log("true");
+if (c === 15 || d === 15 || (d + c) === 15) {
+    console.log(true);
 }
 
 //8. Check whether one of two integers is a multiple of 7 or 11. If so, print true.
@@ -119,9 +125,13 @@ if ((c === 15) || (d === 15) || ((d + c) === 15)) {
 let myNum4 = 49;
 let myNum5 = 10;
 
-if ((myNum4 % 7 === 0) || (myNum5 % 11 === 0)) {
-    console.log("true");
+if ((myNum4 % 7 === 0) || (myNum5 % 11 === 0) ||  (myNum5 % 7 === 0) || (myNum4 % 11 === 0)) {
+    console.log(true);
 }
+
+// if (!((myNum4 % 7) || (myNum5 % 11) ||  (myNum5 % 7) || (myNum4 % 11))) {
+//     console.log("does it work?");
+// }
 
 //9. Calculate the sum of the two given integers. If the two values are the same, then print triple their sum.
 
@@ -131,6 +141,12 @@ if (myNum4 === myNum5) {
     console.log(myNum4 + myNum5);
 }
 
+let sum2 = myNum4 + myNum5;
+
+if (myNum4 === myNum5) {
+    console.log(sum2 * 3);
+} 
+
 //10. Calculate the difference between a specified number and 19. Print double the difference if the specified number is greater than 19.
 
 if (myNum1 > 19) {
@@ -139,12 +155,19 @@ if (myNum1 > 19) {
     console.log(myNum1 - 19);
 }
 
+//OR
+
+let myNum10 = myNum1 - 19;
+if (myNum1 > 19) {
+    console.log(myNum10 * 2);
+}
+
 //11. BONUS CHALLENGE: Make a variable for firstName and age and give each variable values. Create an if/else statement to check whether the person's age is less than 13. If so, print "firstName is a child". If the age is equal to or more than 13 and less than 20, print "firstName is a teenager". If the person's age is equal to and more than 20 and less than 30, then print "firstName is a young adult". If none of these conditions apply, print "firstName is a adult".
 
 let firstName = "Aria";
 let age = 82;
 
-if (age < 18) {
+if (age < 13) {
     console.log(`${firstName} is a child`);
 }
 
