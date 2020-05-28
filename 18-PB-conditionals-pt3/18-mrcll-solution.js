9d88b2cf063f60369404fb2102c25904148e2fcd
mrcll <marcell.csokas@pm.me>
2020-05-28 10:56:27 +0200

    18 mrcll solution

---
+++ 18-PB-conditionals-pt3/18-mrcll-solution.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let aleezaScoreOne = 89;
+let aleezaScoreTwo = 120;
+let aleezaScoreThree = 103;
+
+let lizScoreOne = 116;
+let lizScoreTwo = 94;
+let lizScoreThree = 123;
+
+let maryScoreOne = 97;
+let maryScoreTwo = 134;
+let maryScoreThree = 105;
+
+let aleezaAverage = (aleezaScoreOne + aleezaScoreTwo + aleezaScoreThree) / 3;
+let lizAverage = (lizScoreOne + lizScoreTwo + lizScoreThree) / 3;
+let maryAverage = (maryScoreOne + maryScoreTwo + maryScoreThree) / 3;
+
+
+if (aleezaAverage > lizAverage) {
+    console.log(`The winner is Aleeza's team, with an average score of ${aleezaAverage}.`);
+} else if (aleezaAverage === lizAverage) {
+    console.log(`It's a draw with an average score of ${aleezaAverage}`);
+} else {
+    console.log(`The winner is Liz's team, with an average score of ${lizAverage}.`);
+}
+
+//1.d 
+
+if (aleezaAverage > lizAverage) {
+    if (aleezaAverage > maryAverage) {
+        console.log(`The winner is Aleeza's team, with an average score of ${aleezaAverage}.`);
+    } else {
+        console.log(`The winner is Mary's team, with an average score of ${maryAverage}.`);
+    }
+} else if (aleezaAverage === lizAverage) {
+    if (aleezaAverage === maryAverage) {
+        console.log(`It's a draw with an average score of ${aleezaAverage}`);
+    }
+} else {
+    if (lizAverage > maryAverage) {
+        console.log(`The winner is Liz's team, with an average score of ${lizAverage}.`);
+    } else {
+        console.log(`The winner is Mary's team, with an average score of ${maryAverage}.`);
+    }
+}
