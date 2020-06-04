985968b1cbc3c401a46948cae6d3b8a92d0b4358
mrcll <marcell.csokas@pm.me>
2020-06-04 11:23:16 +0200

    not a perfect rhombus, but it's a rhombus :D

--- 23-PB-nested-loops/23-mrcll-solution.js
+++ 23-PB-nested-loops/23-mrcll-solution.js
@@ -1,13 +1,46 @@
 'use strict';
 
+// UPSIDE DOWN XMAS THREE
 
 
-for (let i = -10; i <= 1; i++) {
+console.log(`
+UPSIDE DOWN XMAS THREE
+`);
+
+for (let i = -10; i <= -1; i++) {
     let space = " "; // reset 
     for (let j = -i; j < 10; j++) {
         space += " ";
     }
-    for (let k = 0; k > (i * 2) - 1; k--) {
+    for (let k = 0; k > (i * 2) + 1; k--) {
+        space += "*";
+    }
+    console.log(space);
+}
+
+// ROMBUS
+
+console.log(`
+RHOMBUS
+`);
+
+for (let i = 0; i <= 10; i++) {
+    let space = ""; // reset 
+    for (let j = i; j < 10; j++) {
+        space += " ";
+    }
+    for (let k = 0; k < (i * 2) - 1; k++) {
+        space += "*"; // 19 x at the last time
+    }
+    console.log(space); // printed
+}
+
+for (let i = -10; i <= 0; i++) {
+    let space = " "; // reset 
+    for (let j = -i; j < 10; j++) {
+        space += " ";
+    }
+    for (let k = 0; k > (i * 2) + 1; k--) {
         space += "*";
     }
     console.log(space);
