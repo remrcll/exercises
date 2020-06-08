5b9090371eb8ac56bb56fcf82ec48d61e7645410
mrcll <marcell.csokas@pm.me>
2020-06-08 14:13:32 +0200

    locale germanized

--- 26-PB-function-weekend/26-mrcll-solution.js
+++ 26-PB-function-weekend/26-mrcll-solution.js
@@ -1,7 +1,7 @@
 "use strict";
-let givenDate = new Date("2020-06-07");
+let givenDate = new Date("2020-06-08");
 const weekend = (day) => {
-    console.log(day === 6 || day === 0 ? `${givenDate.toLocaleDateString()} is a weekend day.` : `${givenDate.toLocaleDateString()} is NOT a weekend day.`);
+    console.log(day === 6 || day === 0 ? `${givenDate.toLocaleDateString('de-DE')} is a weekend day.` : `${givenDate.toLocaleDateString('de-DE')} is NOT a weekend day.`);
 }
 
 weekend(givenDate.getDay());
