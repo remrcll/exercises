cba261e58150e33ee3a413a07c628112ebbdcf15
mrcll <marcell.csokas@pm.me>
2020-06-08 11:13:14 +0200

    adding variable for string

--- 25-PB-function-date/25-mrcll-solution.js
+++ 25-PB-function-date/25-mrcll-solution.js
@@ -1,6 +1,6 @@
 "use strict";
 const event = (today, futureDate) => {
-    console.log(today < futureDate ? 'Your book to the event for the 9th June 2020 is already guaranteed!' : 'Please add a valid future date');
+    console.log(today < futureDate ? `Your book to the event for the ${futureDate} is already guaranteed!` : `Please add a valid future date`);
 }
 
 event("2020-06-08", "2020-07-09");
