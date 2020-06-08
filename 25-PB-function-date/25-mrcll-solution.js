e2232865f3400f2365cba4d1850df1631602701e
mrcll <marcell.csokas@pm.me>
2020-06-08 12:07:55 +0200

    2 versions

--- 25-PB-function-date/25-mrcll-solution.js
+++ 25-PB-function-date/25-mrcll-solution.js
@@ -5,3 +5,16 @@ const event = (today, futureDate) => {
 }
 
 event(new Date(), new Date("2020-07-09"));
+
+//Another version to get rid of the time and timezone:
+
+let todayYear = new Date().getFullYear();
+let todayMonth = new Date().getMonth();
+let todayDay = new Date().getDay();
+let todayDate = `${todayYear}-${todayMonth}-${todayDay}`;
+
+const event2 = (today, futureDate) => {
+    console.log(today < futureDate ? `Your book to the event for the ${todayDate} is already guaranteed!` : `Please add a valid future date`);
+}
+
+event2 (new Date(todayDate), new Date("2020-07-09"));
