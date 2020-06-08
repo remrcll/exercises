ba8bfb3f6c8a760b47602302a2b40d281d35d01e
mrcll <marcell.csokas@pm.me>
2020-06-08 11:43:41 +0200

    new Date added

--- 25-PB-function-date/25-mrcll-solution.js
+++ 25-PB-function-date/25-mrcll-solution.js
@@ -1,6 +1,7 @@
 "use strict";
+
 const event = (today, futureDate) => {
     console.log(today < futureDate ? `Your book to the event for the ${futureDate} is already guaranteed!` : `Please add a valid future date`);
 }
 
-event("2020-06-08", "2020-07-09");
+event(new Date(), new Date("2020-07-09"));
