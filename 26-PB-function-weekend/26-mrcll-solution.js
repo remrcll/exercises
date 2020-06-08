08cba98e97b05e927d3fb343d6a708d1f710c35c
mrcll <marcell.csokas@pm.me>
2020-06-08 14:02:09 +0200

    added .toLocaleDatetString to make it nicer

--- 26-PB-function-weekend/26-mrcll-solution.js
+++ 26-PB-function-weekend/26-mrcll-solution.js
@@ -1,7 +1,7 @@
 "use strict";
 let givenDate = new Date("2020-06-07");
 const weekend = (day) => {
-    console.log(day === 6 || day === 0 ? `${givenDate} is a weekend day.` : `${givenDate} is NOT a weekend day.`);
+    console.log(day === 6 || day === 0 ? `${givenDate.toLocaleDateString()} is a weekend day.` : `${givenDate.toLocaleDateString()} is NOT a weekend day.`);
 }
 
 weekend(givenDate.getDay());
