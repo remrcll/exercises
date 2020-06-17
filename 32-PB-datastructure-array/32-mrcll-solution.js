32f9058423f430a563d674470e409aa50022b240
mrcll <marcell.csokas@pm.me>
2020-06-17 11:32:14 +0200

    32 solved mrcll

---
+++ 32-PB-datastructure-array/32-mrcll-solution.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const myArray = [1, 2, 3, 4, 5];
+console.log(myArray);
+
+let thirdElement = myArray[2];
+console.log(thirdElement);
+
+myArray[3] = "fourth element";
+console.log(myArray);
