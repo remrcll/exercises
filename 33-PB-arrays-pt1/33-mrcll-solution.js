a912bac71f8d6d3086bf3a96030bdb506481dcfb
mrcll <marcell.csokas@pm.me>
2020-06-17 15:31:21 +0200

    all done exept 13

--- 33-PB-arrays-pt1/33-mrcll-solution.js
+++ 33-PB-arrays-pt1/33-mrcll-solution.js
@@ -10,7 +10,7 @@ console.log(secondItem);
 //2. Change the first item in the array to "Berlin".
 
 euroCities[0] = "Berlin";
-console.log(euroCities); 
+console.log(euroCities);
 
 //3. Print the length of the array "euroCities".
 
@@ -28,14 +28,38 @@ console.log(euroCities);
 
 //6. Bonus: Remove the second and third items from the euroCities array.
 
+euroCities.splice(1, 2);
+console.log(euroCities);
+
 //7. Create another variable named asianCities and assign an array of at least 5 cities to the variable.
-const asianCities = ["Taipei", "Tbilisi", "Bankok", "Kathmandu", "Socotra"];
+const asianCities = ["Taipei", "Tbilisi", "Bangkok", "Kathmandu", "Socotra"];
 
 //8. Bonus: Use an array method to select items 2-4 from the array of asianCities and store this in another variable.
 
+let newAsian = asianCities[1]+ ", " + asianCities[2] + ", " + asianCities[3];
+console.log(newAsian);
+
+let newAsian2 = asianCities.slice(1) + asianCities.slice(5);
+
 //9. Bonus: Use a method to concat euroCities with asianCities. Store the result in a variable (eg. worldCities).
 
-//10. Bonus: Use a method to concat euroCities with asianCities. Store the result in a variable (eg. worldCities).
-
-let worldCities = euroCities + asianCities;
+let worldCities = euroCities.concat(asianCities); 
 console.log(worldCities);
+
+//10. Reverse the order of worldCities.
+
+worldCities.reverse();
+
+// 11 .Bonus: Replace the 3rd item in the array of worldCities with "Toronto".
+
+worldCities[2] = "Toronto";
+console.log(worldCities);
+
+
+// 12 . Bonus: Remove no elements from the array of worldCities, but insert "Washington" at the 2nd position.
+
+worldCities.splice(1, 0, "Washington");
+console.log(worldCities);
+
+// 13. Bonus Write a program to join all elements of the result (worldCities) into a string. Example: worldCities = ["Berlin", "London", "Bangkok", "Phnom Penh"]; Expected Outputs: "Berlin , London, Bangkok, Phnom Penh" "Berlin+London+Bangkok+Phnom Penh"
+
