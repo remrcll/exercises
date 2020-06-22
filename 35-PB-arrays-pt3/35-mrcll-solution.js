42f7e1e5fd4b02df32b9d7ee3f1484beff0c74e6
mrcll <marcell.csokas@pm.me>
2020-06-22 11:57:29 +0200

    35/2

--- 35-PB-arrays-pt3/35-mrcll-solution.js
+++ 35-PB-arrays-pt3/35-mrcll-solution.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//1. Where Have My Four Letter Words Gone? 
 const isFourLetters = arr => {
     let result = [];
     for (let i = 0; i < arr.length; i++) {
@@ -13,3 +14,15 @@ const isFourLetters = arr => {
 console.log(isFourLetters(["John", "James", "Jack", "Jeanne"])); //➞ ["John", "Jack"]
 console.log(isFourLetters(["Tomato", "Corn", "Lettuce"])); //➞ ["Corn"]
 console.log(isFourLetters(["Dog", "Cat", "Deer"])); //➞ ["Deer"]
+
+//2.
+
+const monthName = num => {
+    let str = "Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec";
+    let split = str.split(' ');
+    return split[num - 1];
+}
+
+console.log(monthName(3)); // ➞ "March"
+console.log(monthName(12)); //➞ "December"
+console.log(monthName(6)); //➞ "June"
