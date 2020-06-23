9f48dd151be1a060f0c30d35059a17fe595906c9
mrcll <marcell.csokas@pm.me>
2020-06-23 14:47:08 +0200

    Bonus factors done

--- 36-PB-arrays-loops/36-mrcll-solution.js
+++ 36-PB-arrays-loops/36-mrcll-solution.js
@@ -31,7 +31,6 @@ console.log(helloFriend(['Friend1', 'Friend2', 'Friend3', 'Friend4', 'Friend5'])
 
 const cityNames = arr => {
     let result = '';
-    let result2 = '';
     for (let i = 0; i < arr.length; i++) {
         result += arr[i] + ', ';
     }
@@ -40,6 +39,14 @@ const cityNames = arr => {
 }
 
 console.log(cityNames(['City1', 'City2', 'City3', 'City4', 'City5']));
+
+//or
+
+const cityNames2 = cities => {
+    const city = cities.join(", ");
+    return city;
+};
+console.log(cityNames2(["Berlin", "Tbilisi", "Barselona", "Vilnius"]));
 
 //4. Odds and Evens. Create a program that changes a given array by adding 1 to each odd integer and subtracting 1 from each even integer.
 
@@ -89,6 +96,41 @@ console.log(noDuplicates([1, 6, 6, 9, 9])); // ➞ [1, 6, 9]
 console.log(noDuplicates([2, 2, 2, 2, 2, 2])); // ➞ [2]
 console.log(noDuplicates([5, 10, 15, 20, 25])); // ➞ [5, 10, 15, 20, 25]
 
+//Or
+
+function noDuplicates2(array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (newArray.indexOf(array[i]) === -1) {
+            // we don't have it already
+            newArray.push(array[i]);
+            // we push it once!
+        }
+    }
+    return newArray;
+}
+console.log(noDuplicates2([1, 6, 6, 9, 9])); // ➞ [1, 6, 9]
+console.log(noDuplicates2([2, 2, 2, 2, 2, 2])); // ➞ [2]
+console.log(noDuplicates2([5, 10, 15, 20, 25])); // ➞ [5, 10, 15, 20, 25]
+
+//or 
+
+function noDuplicates3(array) {
+    let newArray = array;
+    for (let i = 0; i < array.length; i++) {
+        if (newArray.indexOf(newArray[i]) !== newArray.lastIndexOf(newArray[i])) {
+            newArray.splice(i, 1);
+            i = 0;
+        }
+    }
+    return newArray;
+}
+console.log(noDuplicates3([1, 4, 4, 7, 7, 7]));
+console.log(noDuplicates3([1, 6, 6, 6, 9, 9]));
+console.log(noDuplicates3([2, 2, 2, 2, 2, 2]));
+console.log(noDuplicates3([5, 10, 15, 20, 25]));
+
+
 //7. Repeat it. Create a program with two variables: "item" and "times". Create a program that repeats the "item" as many times as specified by "times". The first variable ("item") is the item that needs repeating while the second argument ("times") is the number of times the item is to be repeated. Print the result in an array.
 
 const repeatIt = (item, times) => {
@@ -100,3 +142,22 @@ const repeatIt = (item, times) => {
 }
 
 console.log(repeatIt("example", 3)); // ➞ ["example", "example", "example"]
+
+//Bonus: Factors. A factor chain is an array where each previous element is a factor of the next consecutive element. The following is a factor chain: [3, 6, 12, 36]
+
+const factor = arr => {
+    let result;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] % arr[i - 1] === 0) {
+            result = true;
+        } else {
+            result = false;
+        }
+    }
+    return result;
+}
+
+console.log(factor([1, 2, 4, 8, 16, 32])); // true
+console.log(factor([1, 1, 1, 1, 1, 1])); // true
+console.log(factor([2, 4, 6, 7, 12])); // false
+console.log(factor([10, 1])); // false
