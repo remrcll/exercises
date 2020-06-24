173acb93239eb446d7909a65d8c452cc3f432fc4
mrcll <marcell.csokas@pm.me>
2020-06-24 10:10:23 +0200

    added other solutions

--- 37-PB-arrays-nested-loops/37-mrcll-solution.js
+++ 37-PB-arrays-nested-loops/37-mrcll-solution.js
@@ -15,6 +15,35 @@ const tree = (arr) => {
 
 tree(['*', '*', '*', '*']);
 
+// Martinas solution: build a half pyramid with return
+const halfPyramid = (lines) => {
+    let array = [];
+    for (let i = 0; i < lines; i++) {
+        for (let j = 0; j <= i; j++) {
+            array.push("*"); // the amount of stars per line
+            //console.log(array);
+        }
+        array.push("\n"); // insert a new line break special character
+    }
+    return " " + array.join(" ");
+}
+console.log("Task 1: Pyramid");
+console.log(halfPyramid(4));
+// mohameds number pyramid
+function mohamedsPyramid(lines) {
+    let string = '';
+    for (let i = 0; i < lines; i++) { // forwards
+        for (let j = i; j >= 0; j--) { // backwards accordingly to i
+            string += j + " ";
+        }
+        string += "\n"; // new line break
+        //console.log(string);
+    }
+    return string;
+}
+console.log(mohamedsPyramid(10));
+
+
 //2. Write a program which prints the elements of the following array:
 
 let arr = [
@@ -28,7 +57,7 @@ let arr = [
 for (let i = 0; i < arr.length; i++) {
     console.log(`row ${i}`);
     for (let j = 0; j < arr[i].length; j++) {
-        console.log(arr[i][j]);
+        console.log(" " + arr[i][j]);
     }
 }
 
@@ -55,3 +84,21 @@ console.log(third2([
     [1, 2, 3, 4],
     [0, 1, 2, 3, 4]
 ]));
+
+//Vincenzo solution: 
+
+const printNumbers = () => {
+    let arrayOne = [];
+    let arrayTwo = [];
+    for (let i = 1; i <= 4; i++) {
+        for (let j = 0; j <= 4; j++) {
+            if (j !== 0 && j !== 4) {
+                arrayOne.push(i);
+            }
+            arrayTwo.push(j);
+        }
+    }
+    console.log(arrayOne.join(" "));
+    console.log(arrayTwo.join(" "));
+}
+printNumbers();
