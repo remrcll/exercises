92336eb687c6e7d642db549b04c50bfe1aca5fae
mrcll <marcell.csokas@pm.me>
2020-06-24 15:11:26 +0200

    38 done mrcll

---
+++ 38-PB-datastructure-objectscope/38-mrcll-solution.js
@@ -0,0 +1,40 @@
+"use strict";
+
+console.log(`1. Create the following object:
+`);
+
+const student = {
+    firstName: "John",
+    lastName: "Smith",
+    class: 12,
+    whoIsJohnSmith: function () { /// METHOD  ==> FUNCTION INSIDE OF AN OBJECT 
+        return `${this.firstName} ${this.lastName} is a student in class ${this.class}.`;
+    }
+};
+
+const callWhoIsJohnSmith = student.whoIsJohnSmith.bind(student);
+console.log(callWhoIsJohnSmith());
+
+console.log(`
+2. Create a person object. Include the person's first and last name, age, job, city etc. Create a method to print data from the object e.g. "John Smith is a 41 year old engineer living in France".
+`);
+
+const person = {
+    firstName: "John",
+    lastName: "Smith",
+    age: 41,
+    profession: "engineer",
+    from: "France",
+    whoIsThisPerson: function () { 
+        return `${this.firstName} ${this.lastName} is a ${this.age} year old ${this.profession} living in ${this.from}.`;
+    }
+};
+
+const callWhoIsThisPerson = person.whoIsThisPerson.bind(person);
+console.log(callWhoIsThisPerson());
+
+console.log(`
+Write a method to get the length of the person object.
+`);
+
+console.log(Object.keys(person).length);
