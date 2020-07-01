a31a4e72260657e952a163a128519174773588c0
mrcll <marcell.csokas@pm.me>
2020-07-01 12:47:26 +0200

    43-done

--- 43-PB-datastructure-advarray/43-mrcll-solution.js
+++ 43-PB-datastructure-advarray/43-mrcll-solution.js
@@ -18,8 +18,8 @@ const orders = [{
 // console.log(Object.values(orders[]));
 let amounts = [];
 for (let key in orders) {
-    let value = orders[key];
-    let result = amounts.push(value.amount);
+    // let value = orders[key].amount;
+    amounts.push(orders[key].amount);
 }
 
 const sum = amounts.reduce(function (previousValue, currentValue) {
@@ -28,15 +28,38 @@ const sum = amounts.reduce(function (previousValue, currentValue) {
 
 console.log(sum);
 
+// Mohammeds version: 
+
+const total = orders.reduce(function (previousValue, currentValue) {
+    return currentValue["amount"] + previousValue;
+}, 0);
+console.log(total);
+
 //2.
 
-const arrayOfNumbers = [3, 45, 6, 56, 7, 9]; 
+const arrayOfNumbers = [3, 45, 6, 56, 7, 9];
 
 const newArray = arrayOfNumbers.map((value) => {
-    return value + 1; 
+    return value + 1;
 });
 
 console.log(newArray);
+
+// 3. Filter Evens
+
+const filterEvens = value => value.filter(value => value % 2 === 0);
+
+console.log(filterEvens([1, 2, 3, 11, 12, 13]));
+console.log(filterEvens([22, 2, 31, 110, 6, 13]));
+
+//4. Filter Friends
+
+const friends = ["rika", "jenna", "bleda", "oliver", "itamar"];
+const filterItems = (listOfFriends, lookFor) => listOfFriends.filter(listOfFriends => listOfFriends.includes(lookFor));
+
+
+console.log(filterItems(friends, 'ka')); // ["Rika"];
+console.log(filterItems(friends, 'e')); // ["Jenna", "Bleda", "Oliver"];
 
 // 5. 
 const sum3 = arr => arr.reduce((previous, current) => previous + current);
