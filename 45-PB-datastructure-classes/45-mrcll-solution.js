3cc0c52a35a1ca9e2a5283e1147d9b0874b72f84
mrcll <marcell.csokas@pm.me>
2020-07-02 16:10:33 +0200

    updated with Vincenzo's code

--- 45-PB-datastructure-classes/45-mrcll-solution.js
+++ 45-PB-datastructure-classes/45-mrcll-solution.js
@@ -35,33 +35,67 @@ console.log(newCylinder.toVolume());
 
 //4. 
 
+// my code:
+// class Tv {
+//     constructor(brand = "Samsung", channel, volume) {
+//         this.brand = brand;
+//         this.channel = channel;
+//         this.volume = volume;
+//         this.changeVolume = function () {
+//             if (this.volume >= 0 && this.volume <= 100) {
+//                 return `The volume is set to ${this.volume}`;
+//             } else {
+//                 return `${volume} is an invalid number. Please set the volume between 0 and 100.`
+//             }
+            
+//         }
+//         this.changeChannel = function () {
+//             let randomChannel = Math.floor((Math.random() * 50) + 1);
+//             return `Your random channel is ${randomChannel}.`
+//         }
+
+//         this.reset = function () {
+//             return `Your TV is reset to the default values: channel: = ${this.channel}, brand = ${this.brand}, volume = ${this.volume}!`
+//         }
+//     }
+// };
+
+// const newSettings = new Tv("Samsung", 8, 60); // would come out of the database
+// console.log(newSettings);
+// console.log(newSettings.changeVolume());
+// console.log(newSettings.changeChannel());
+// console.log(newSettings.reset());
+
+//Vincenzo'S code 
 
 class Tv {
-    constructor(brand, channel, volume) {
+    constructor(brand) {
         this.brand = brand;
-        this.channel = channel;
-        this.volume = volume;
-        this.changeVolume = function () {
-            if (this.volume >= 0 && this.volume <= 100) {
-                return `The volume is set to ${this.volume}`;
-            } else {
-                return `${volume} is an invalid number. Please set the volume between 0 and 100.`
-            }
-            
-        }
-        this.changeChannel = function () {
-            let randomChannel = Math.floor((Math.random() * 50) + 1);
-            return `Your random channel is ${randomChannel}.`
-        }
-
-        this.reset = function () {
-            return `Your TV is reset to the default values: channel: = ${this.channel}, brand = ${this.brand}, volume = ${this.volume}!`
+        this.channel = 1;
+        this.volume = 50;
+    }
+    increaseVolume = () => {
+        if (this.volume >= 100) {
+            console.log("Max volume reached!");
+        } else {
+            this.volume += 10;
+            console.log("Volume up!", this.volume);
         }
     }
-};
+    decreaseVolume = () => {
+        if (this.volume <= 0) {
+            console.log("Min volume reached!");
+        } else {
+            this.volume -= 10;
+            console.log("Volume Down!", this.volume);
+        }
+    }
+    randomZapping = () => this.channel = Math.floor(Math.random() * 50);
+    resetTv = () => {
+        this.volume = 50;
+        this.channel = 1;
+    }
+    getTvStatus = () => `${this.brand} at channel ${this.channel}, volume ${this.volume}`;
+}
+const myTv = new Tv("Samsung");
 
-const newSettings = new Tv("RBB", 1, 50); // would come out of the database
-console.log(newSettings);
-console.log(newSettings.changeVolume());
-console.log(newSettings.changeChannel());
-console.log(newSettings.reset());
