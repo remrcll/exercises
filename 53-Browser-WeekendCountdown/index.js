9ab7fcdbff0b7f865df008c0091baf36ce716883
mrcll <passes@protonmail.com>
2020-07-15 10:31:03 +0200

    finished without styling

---
+++ 53-Browser-WeekendCountdown/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let container = document.querySelector(".container")
+let changeThis = document.querySelector(".change-this")
+let buttonToListen = document.querySelector("button")
+let input = document.querySelector(".addme")
+let d = new Date();
+let dayInNumber = d.getDay();
+let weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+let n = weekday[d.getDay()];
+let leftDays = 7 - dayInNumber;
+
+//event handler
+function handler(event) {
+    changeThis.innerHTML = `Hello ${input.value}! Today is ${n}. Only ${leftDays} days left until Weekend!`
+    input.value = ""
+    /*  button.innerHTML="changed" */
+}
+
+//attaching event listener on button
+buttonToListen.addEventListener("click", handler)
